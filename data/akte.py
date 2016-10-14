e907bcfe9456a5dcd569e71f56b3babb02ab3b0a
Tjibbe Hofkamp <t_w_h@hotmail.com>
2016-10-14 09:02:00 +0200

    Add files via upload

--- data/akte.py
+++ data/akte.py
@@ -1,3 +1,20 @@
+# akte.py - library to do something with akte objects
+#
+#   Copyright (C) 2016 T.Hofkamp
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import sqlite3
 
 class aktepersoon:
@@ -377,3 +394,34 @@ class akte:
         if self.eventplace:
             header += " te "+self.eventplace
         return [header,""]
+
+class aktecache:
+    def __init__(self,cur):
+        self.aktes = {}
+
+        cur.execute('''SELECT * FROM akte''')
+        for row in cur:
+            # select from personakte bla
+            ret = row[1]
+            for i in row[2::]:
+                if i:
+                    ret += '|' + str(i)
+                else:
+                    ret += '|'
+            self.aktes[row[0]] = ret
+
+        cur.execute('''SELECT * FROM aktepersoon''')
+        for row in cur:
+            if row[1]:
+                ret = row[1]
+            else:
+                ret = ""
+            for i in row[2::]:
+                if i:
+                    ret += '|' + str(i)
+                else:
+                    ret += '|'
+            self.aktes[row[0]] += "#" + ret
+
+    def flush(self):
+        self.aktes = {}
